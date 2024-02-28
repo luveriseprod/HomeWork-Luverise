4f66cb5fc791a42973653111cf4d61bfabd71e75
LRDev <93220194+luveriseprod@users.noreply.github.com>
2024-02-28 23:09:31 +0600

    DOM v3.0

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-let articles = document.getElementById("allArticles")
-let countsArticles = document.querySelectorAll(".section2 article").length
-articles.innerText = `${articles.innerText} (${countsArticles} шт.)`
+let articles = document.getElementsByClassName("section2")
+let formContact = document.getElementsByClassName("section3")
+let goAllArticlesBtn = document.getElementById("goAllArticles")
+let goContactsBtn = document.getElementById("goContacts")
+goAllArticlesBtn.addEventListener("click", () => {
+    articles[0].scrollIntoView({behavior: "smooth"})
+})
+goContactsBtn.addEventListener("click", () => {
+    formContact[0].scrollIntoView({behavior: "smooth"})
+})
