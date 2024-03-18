77e5e16f1239b7885bc1ba99d6b69089b2d732d9
LRDev <93220194+luveriseprod@users.noreply.github.com>
2024-03-18 15:40:01 +0300

    DOM v4.0

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let allArticlesBtn = document.getElementById("allArticles");
 let contactsBtn = document.getElementById("contacts");
+
 let articles = document.getElementById("section2");
 let formContacts = document.getElementById("formContacts");
 
-allArticlesBtn.addEventListener("click", () => {
-    articles.scrollIntoView({behavior: "smooth"})
-})
-contactsBtn.addEventListener("click", () => {
-    formContacts.scrollIntoView({behavior: "smooth"})
-})
+function addScrollToElement(btn, el){
+    btn.addEventListener("click", () => {
+        el.scrollIntoView({behavior: "smooth"})
+    })
+}
+
+addScrollToElement(allArticlesBtn, articles)
