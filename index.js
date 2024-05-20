06ba1a7fe53300afb43f8f4ba72bd28199c97821
LRDev <93220194+luveriseprod@users.noreply.github.com>
2024-05-20 22:04:36 +0600

    DOM v5

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-let allArticlesBtn = document.getElementById("allArticles");
-let contactsBtn = document.getElementById("contacts");
+const userArticles = [
+  {
+      name: "Javascript",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus minima fugit voluptatem odio maxime adipisci!.",
+      link: "javascript.html",
+  },
+  {
+      name: "Technology",
+      text: "Corrupti enim ipsa, praesentium, autem natus illo voluptat.",
+      link: "technology.html",
+  },
+];
 
-let articles = document.getElementById("section2");
-let formContacts = document.getElementById("formContacts");
+let body = document.body.children[4]
+let newSection = document.createElement("section")
+newSection.className = "section2"
+body.after(newSection)
 
-function addScrollToElement(btn, el){
-    btn.addEventListener("click", () => {
-        el.scrollIntoView({behavior: "smooth"})
-    })
-}
-
-addScrollToElement(allArticlesBtn, articles)
+let articles = userArticles.map(info => {
+  let {name, text, link} = info;
+  let newArticles = document.createElement("div");
+  newArticles.innerHTML = `${name} ${text} ${link}`
+  newSection.appendChild(newArticles)
+  return newArticles;
+})
+console.log(articles);
