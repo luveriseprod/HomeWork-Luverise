59d58ff21935c8f63c766dcdba407546a2d7bfcc
LRDev <93220194+luveriseprod@users.noreply.github.com>
2024-05-22 23:48:39 +0600

    DOM v6 import

---
+++ articles.js
@@ -0,0 +1,18 @@
+export const userArticles = [
+    {
+      name: "Javascript",
+      text: "JavaScript often abbreviated as JS, is a programming language and core technology of the Web, alongside HTML and CSS. 99% of websites use JavaScript on the client side for webpage behavior.",
+      link: "javascript.html",
+    },
+    {
+      name: "MongoDB",
+      text: "MongoDB is a source-available, cross-platform, document-oriented database program. Classified as a NoSQL database product, MongoDB utilizes JSON-like documents with optional schemas.",
+      link: "mongodb.html",
+    },
+    {
+      name: "React",
+      text: "React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.",
+      link: "react.html"
+    }
+  ];
+  
--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-const userArticles = [
-  {
-    name: "Javascript",
-    text: "JavaScript often abbreviated as JS, is a programming language and core technology of the Web, alongside HTML and CSS. 99% of websites use JavaScript on the client side for webpage behavior.",
-    link: "javascript.html",
-  },
-  {
-    name: "MongoDB",
-    text: "MongoDB is a source-available, cross-platform, document-oriented database program. Classified as a NoSQL database product, MongoDB utilizes JSON-like documents with optional schemas.",
-    link: "mongodb.html",
-  },
-  {
-    name: "React",
-    text: "React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.",
-    link: "react.html"
-  }
-];
-
+import { userArticles } from "./articles.js";
 let artSection = document.getElementById("articles");
 
 let addArticles = userArticles.map((info) => {
@@ -49,3 +32,5 @@ function addScrollToElement(btn, el) {
 
 addScrollToElement(allArticlesBtn, articles);
 addScrollToElement(contactsBtn, formContacts);
+
+
