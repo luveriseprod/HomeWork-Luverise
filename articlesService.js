259e7a206cb5e92b1f6c1870042801ee04bb7058
LRDev <93220194+luveriseprod@users.noreply.github.com>
2024-05-28 22:23:02 +0600

    find_id

--- articlesService.js
+++ articlesService.js
@@ -1,6 +1,19 @@
 import userArticles from "./articles.json" assert { type: "json" };
 
+function getArticlesId(id) {
+  const getArticlesId = userArticles.articles.find(art => {
+  return art.id === Number(id)
+  })
+}
+console.log(getArticlesId(1));
+
 export default function getArticles() {
   let articles = userArticles.articles;
   return articles;
 }
+
+// const currentUser = userData.users.find(user => {
+//   return user.id === Number(userId)
+// })
+
+// console.log(currentUser);
